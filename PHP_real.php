976beedadf53ec100d166cd5d18bd3ad974538f3
Andrey Gomzin <andgomzin@gmail.com>
2021-07-13 16:21:08 +0500

    new_code_file_added

---
+++ PHP_real.php
@@ -0,0 +1,20 @@
+﻿$root = $this->GetRootActivity();
+$quantity = $root->GetVariable('Variable1');
+$seria = $root->GetVariable('random_string');
+$i = 0;
+while ($i < $quantity) {
+    $i = $i + 1;
+    $entity = new CCrmLead; 
+    $fields = array( 
+        'TITLE' => 'Лид-терминатор серии '.$seria.', номер '.$i,
+        'STATUS_ID' => 'PROCESSED',
+        'COMMENTS' => 'Это лид из массового спавна лидов. Скоро они захватят этот портал.',
+        'SOURCE_DESCRIPTION' => 'СЛАВА ЛИДАМ!!!',
+        'UF_CRM_SORT_FIELD' => '50',
+        'UF_CRM_NUNBER' => $i,
+        'UF_CRM_SERIA' => $seria,
+    ); 
+    echo $entity->add($fields);
+};
+
+$this -> SetVariable('Variable2', $i);
